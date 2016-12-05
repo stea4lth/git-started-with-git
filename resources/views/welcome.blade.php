28ef6215cae9183de543b46bb9055933cb6c64ef
cmac <nowlaninc@gmail.com>
2016-12-05 00:19:46 -0500

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel asdfasdf
                 </div>
 
                 <div class="links">
